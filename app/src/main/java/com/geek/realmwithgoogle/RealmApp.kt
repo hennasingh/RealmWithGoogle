519fc60d6ea78ef7263c3c3ecbb880a92486e564
Henna Singh <hennasingh04@gmail.com>
2020-11-16 16:12:45 +0000

    inplemented layout and global Realm App configuration

---
+++ app/src/main/java/com/geek/realmwithgoogle/RealmApp.kt
@@ -0,0 +1,20 @@
+package com.geek.realmwithgoogle
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.mongodb.App
+import io.realm.mongodb.AppConfiguration
+
+const val appId ="realmsignin-abyof"
+lateinit var app: App
+
+class RealmApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+
+        app = App(AppConfiguration.Builder(appId).build())
+
+    }
+}
